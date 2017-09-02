715bab7079abfcd521bfac65a02e9761cbdd7fb5
Ishan Manandhar <ishan.manandhar@wolfmatrix.com>
2017-09-02 15:41:21 +0545

    including header footer and description

---
+++ new_theme/wp-content/themes/demo_theme/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+  </head>
+  <h2><?php bloginfo('description'); ?></h2>
+  <body>
